273f933b90a3bb333f0629295cd282026b33d144
Jan  Matthias Pilzer <jan.matthias.pilzer@sap.com>
2016-02-11 21:23:50 +0100

    50k

--- round1/dron.py
+++ round1/dron.py
@@ -5,9 +5,20 @@ class Drone:
     def __init__(self, x, y):
         self.x = x
         self.y = y
+        self.isFree = True
+        
+    def tick(self, turn):
+        if self.isFree:
+            return True
+        self.isFree = turn == self.free_at
+        return self.isFree
+    
+    def isIdle(self):
+        return self.isFree
     
     def to(self, x, y, turn):
         self.free_at = turn + math.ceil(math.sqrt(math.pow(abs(self.x - x), 2) + math.pow(abs(self.y - y), 2))) + 1
         self.x = x
         self.y = y
+        self.isFree = False
         return self.free_at
--- round1/easy.py
+++ round1/easy.py
@@ -3,31 +3,32 @@ from round1.parse import parse_file
 from round1.printer import printLoad, printDeliver, printToFile
 
 
-n_rows, n_cols, n_drones, deadline, max_load, p, products, warehouse, warehouses, o, orders = parse_file('data/test.in')
+n_rows, n_cols, n_drones, deadline, max_load, p, products, warehouse, warehouses, o, orders = parse_file('data/busy_day.in')
 
-drone = Drone(warehouses[0]["cords"][0], warehouses[0]["cords"][1])
+drones = []
+for d in range(n_drones):
+    drones.append(Drone(warehouses[0]["cords"][0], warehouses[0]["cords"][1]))
+
 current_oder = 0
 turn = 0
-while turn < deadline:
-    order = orders[current_oder]
-    current_oder += 1
-    for product, count in enumerate(order["products"]):
-        weight = max_load
-        while count > 0 and weight > 0:
-            # Fly to different one
-            for i, warehouse in enumerate(warehouses):
-                if warehouse["products"][product] > 0:
-                    load = min(warehouse["products"][product], count, max_load / products[product])
-                    weight -= load * products[product]
-                    if load > 0:
-                        printLoad(0, i, product, count)
-                        turn += 1
-                        count -= load
-                        warehouse["products"][product] -= load
-                        turn = drone.to(warehouse["cords"][0], warehouse["cords"][1], turn)
-    if load > 0:
-        printDeliver(0, current_oder, product, load)
-        turn += 1
-        turn = drone.to(order["cords"][0], order["cords"][1], turn)
+while turn < deadline and current_oder < len(orders):
+    for drone_i, drone in enumerate(drones):
+        drone.tick(turn)
+        if drone.isIdle():
+            order = orders[current_oder]
+            drone_free = turn
+            for product, count in enumerate(order["products"]):
+                for i, warehouse in enumerate(warehouses):
+                    if warehouse["products"][product] > 0 and count > 0:
+                        warehouse["products"][product] -= 1
+                        count -= 1
+                        drone_free = drone.to(warehouse["cords"][0], warehouse["cords"][1], drone_free)
+                        if (drone_free < deadline): 
+                            printLoad(drone_i, i, product, 1)
+                        drone_free = drone.to(order["cords"][0], order["cords"][1], drone_free)
+                        if (drone_free < deadline): 
+                            printDeliver(drone_i, current_oder, product, 1)
+            current_oder += 1
+    turn += 1
         
 printToFile("busy_day.out")
